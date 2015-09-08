c0f6cbd88305437cca2372a26090d75098455208
joehsieh <ninja31312@gmail.com>
2015-09-08 13:48:26 +0800

    Initial Commit

---
+++ NewsFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsFeed
+//
+//  Created by joehsieh on 2015/9/8.
+//  Copyright (c) 2015年 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsFeed
+//
+//  Created by joehsieh on 2015/9/8.
+//  Copyright (c) 2015年 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
